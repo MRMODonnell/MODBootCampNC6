0a61e27c01f84bc5c613e819ca905a566a8cf553
Michael O'Donnell <michael.odonnell@st-annes.org.uk>
2023-01-16 12:13:58 +0000

    added Js Scripts

---
+++ questions.js
@@ -0,0 +1,86 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Javascript is an _______ language",
+    choices: ["Object-Oriented", "Object-based", "Procedural", "None of the Above"],
+    answer: "Object-based"
+  },
+  {
+    title: "What are the two basic groups of data types in JavaScript?",
+    choices: [
+      "Primitive and attribute",
+      "Primitive and reference types",
+      "Reference types and attribute",
+      "None of the above"
+    ],
+    answer: "Primitive and reference types"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "Boolean operators that can be used in JavaScript include:",
+    choices: [
+      "'And' Operator &&",
+      "'Or' Operator ||",
+      "'Not' Operator !",
+      "All the above"
+    ],
+    answer: "All the above"
+  },
+  {
+    title:
+      "Which of the following keywords is used to define a variable in Javascript",
+    choices: [
+      "Var",
+      "Let",
+      "Both A And B",
+      "None of the Above"
+    ],
+    answer: "Both A And B"
+  },
+  {
+    title: "What is the data type of variables in JavaScript?",
+    choices: [
+      "Object data types",
+      "Function data type",
+      "None of the above",
+      "All of the above"
+    ],
+    answer: "Object data types"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+    choices: ["Boolen", "Undefined", "Object", "Interger"],
+    answer: "Object"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: ["Alert", "Confirm", "Prompt", "All the above"],
+    answer: "All the above"
+  }
+];
